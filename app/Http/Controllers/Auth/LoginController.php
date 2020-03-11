709438d2e1ea587f329a1e659f84cc8b4a7ddf04
Timothy Ododo <timothyododo@gmail.com>
2020-03-11 15:24:26 +0000

    fix error redirecting to dashboard

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProviderDB::DASHBOARD;
+    protected $redirectTo = RouteServiceProvider::DASHBOARD;
 
     /**
      * Create a new controller instance.
